6f5e4d7b3164f6267194174d4ed80f4c865fef9e
seagetch <sigetch@gmail.com>
2024-05-22 15:17:11 +0900

    Added inStopSnapshot function.

--- source/inochi2d/utils/snapshot.d
+++ source/inochi2d/utils/snapshot.d
@@ -17,34 +17,24 @@ Texture inSnapshot(bool forceUpdate = false)(Puppet puppet) {
                 dcomposite.textures[0].dispose();
                 dcomposite.textures[0] = null;
             }
-//            if (dcomposite == puppet.root) {
-//                puppet.root = dcomposite.children[0];
-//            }
         }
         dcomposite = new DynamicComposite();
         snapshotPuppet = puppet;
         dcomposite.name = "Snapshot";
         dcomposite.parent(puppet.getPuppetRootNode());
-//        puppet.root = dcomposite;
         puppet.root.parent(cast(Node)dcomposite);
         dcomposite.setPuppet(puppet);
     }
-//    dcomposite.scanSubParts([puppet.getPuppetRootNode()]);
     puppet.rescanNodes();
     dcomposite.setupSelf();
     dcomposite.autoResizedMesh = false;
     dcomposite.draw();
-    /*
-    dcomposite.setPuppet(snapshotPuppet);
-    foreach (child; puppet.getRootParts()) {
-        dcomposite.setupChild(child);
-    }
-    dcomposite.scanSubParts(puppet.getRootParts());
-    dcomposite.setupSelf();
-    dcomposite.autoResizedMesh = false;
-    dcomposite.invalidate();
-    dcomposite.drawContents();
-    */
     auto tex = dcomposite.textures[0];
     return tex;
+}
+
+void inStopSnapshot(Puppet puppet) {
+    if (puppet.actualRoot != puppet.root) {
+        puppet.root.parent(puppet.getPuppetRootNode());
+    }
 }
