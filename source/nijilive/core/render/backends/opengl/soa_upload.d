3a21959dbd1d146157c6c5a9ccf37823a7fe500c
seagetch <sigetch@gmail.com>
2025-11-15 14:36:40 +0900

    Restored uploadDeform, uploadVertices to right implementation.

--- source/nijilive/core/render/backends/opengl/soa_upload.d
+++ source/nijilive/core/render/backends/opengl/soa_upload.d
@@ -6,7 +6,6 @@ import bindbc.opengl;
 import nijilive.math : veca;
 import std.traits : Unqual;
 import nijilive.core.render.profiler : profileScope, RenderProfileScope;
-import nijilive.core.render.backends.opengl.buffer_sync : waitForBuffer;
 
 private template VecInfo(Vec) {
     static if (is(Unqual!Vec == veca!(float, N), size_t N)) {
@@ -22,32 +21,14 @@ private template VecInfo(Vec) {
 void glUploadFloatVecArray(Vec)(GLuint buffer, auto ref Vec data, GLenum usage, string profileLabel = null)
 if (VecInfo!Vec.isValid) {
     if (buffer == 0 || data.length == 0) return;
-    if (profileLabel.length)
-        waitForBuffer(buffer, profileLabel ~ ".Wait");
-    RenderProfileScope totalScope;
-    if (profileLabel.length)
-        totalScope = profileScope(profileLabel ~ ".Total");
-    enum laneCount = VecInfo!Vec.laneCount;
-    auto laneBytes = cast(size_t)data.length * float.sizeof;
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    const size_t totalBytes = laneBytes * laneCount;
     auto raw = data.rawStorage();
-    debug {
-        assert(raw.length == laneCount * data.length);
-    }
     if (raw.length == 0) return;
-    {
-        RenderProfileScope orphanScope;
-        if (profileLabel.length)
-            orphanScope = profileScope(profileLabel ~ ".Orphan");
-        glBufferData(GL_ARRAY_BUFFER, totalBytes, null, usage);
+    RenderProfileScope profScope = RenderProfileScope.init;
+    if (profileLabel.length) {
+        profScope = profileScope(profileLabel);
     }
-    {
-        RenderProfileScope copyScope;
-        if (profileLabel.length)
-            copyScope = profileScope(profileLabel ~ ".Copy");
-        glBufferSubData(GL_ARRAY_BUFFER, 0, totalBytes, raw.ptr);
-    }
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, raw.length * float.sizeof, raw.ptr, usage);
 }
 
 } else {
